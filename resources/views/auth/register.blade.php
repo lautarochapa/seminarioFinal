d676e1b1b029ad34ef6edc3d99d68eeb2c7af09a
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-02 21:18:00 -0300

    registro con google

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="623128501385-5iaciaqn2e29igc5j9vrim31i1mnj3oa.apps.googleusercontent.com">
+
+
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+
+<script>
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+
+
+</script>
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
