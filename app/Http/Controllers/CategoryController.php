ec0613b96479c1e235d4c8d2fc2de0d2731ab0bc
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-03 01:50:19 -0300

    init niveles de acceso

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,34 +19,17 @@ class CategoryController extends Controller
 
 
       $array = Category::all();
-   
-
-
-
-
-// funcion find father recibe el arreglo y el id por defecto 0
+      
 function ffather($arr,$el=0){
-  // creamos una varible final que contendra nuestros hijos
   $final=array();
-  // recorremos el arreglo
   foreach ($arr as $key => $value) {
-      // validamos que el id actual coincida con el id_padre "encontramos un hijo"
       if ($el == $value["padre"]){
-          // volvemos a llamar a la funcion find father para buscar ahora los hijos de los hijos
           $value["hijos"]=ffather($arr,$value["id"]);
-          // cargamos el nuevo valor en $final
-          $final[]= $value['nombre'];
+          $final[]= $value;
       }
   }
-  // retornamos $final
   return $final;
 }
-
-
-
-
-
-
         return ffather($array);
       }
 
