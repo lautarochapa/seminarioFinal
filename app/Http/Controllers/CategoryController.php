1552c279fccdffcce30f712f29ad4d83b5dbe968
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-03 02:22:10 -0300

    swagger api

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,8 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 
+
+/**
+* @OA\Info(title="API Usuarios", version="1.0")
+*
+* @OA\Server(url="http://swagger.local")
+*/
+
 class CategoryController extends Controller
 {
+
+    /**
+    * @OA\Get(
+    *     path="/api/users",
+    *     summary="Mostrar usuarios",
+    *     @OA\Response(
+    *         response=200,
+    *         description="Mostrar todos los usuarios."
+    *     ),
+    *     @OA\Response(
+    *         response="default",
+    *         description="Ha ocurrido un error."
+    *     )
+    * )
+    */
+
+
     function getAll(){
         return Category::all();
       }
