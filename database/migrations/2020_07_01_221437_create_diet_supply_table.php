393b3188656d6c04039b87cba843369cb3ded789
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-04 19:42:18 -0300

    arreglos en el der

--- database/migrations/2020_07_01_221437_create_diet_supply_table.php
+++ database/migrations/2020_07_01_221437_create_diet_supply_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDietProductTable extends Migration
+class CreateDietSupplyTable extends Migration
 {
     /**
      * Run the migrations.
