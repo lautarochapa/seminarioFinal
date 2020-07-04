511ab4901dbf2e58f68e00352d9b492fd39f3e67
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-04 17:53:59 -0300

    armado de templates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-    <div class="content">
+        <div class="col-md-8">
                 <div class="title m-b-md">
                     CocinaComidaControl
                 </div>
