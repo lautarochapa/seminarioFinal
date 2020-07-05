341b1ace2bcf0d226d82088f2bb22981cba0b8ba
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-05 17:16:24 -0300

    api + vistas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
       function getAll(){
 
 
-        $products = Product::with('brand', 'supply')->paginate(15)->get();
+        $products = Product::with('brand', 'supply')->get();
 
         return response()->json([
 
