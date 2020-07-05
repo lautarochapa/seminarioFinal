552a4a5efe61b5ed4d4c7a6aa4fa7be2ffc44a9d
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-05 03:18:33 -0300

    api productos

--- public/js/app.js
+++ public/js/app.js
@@ -38208,7 +38208,11 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_c("tree", { attrs: { "tree-data": _vm.categorias } })], 1)
+  return _c(
+    "div",
+    [_c("tree", { attrs: { "tree-data": _vm.data.categorias } })],
+    1
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
