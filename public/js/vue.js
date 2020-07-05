eb92b768f3dbce1b5adcafdc3de990620a39b793
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-04 22:46:01 -0300

    api productos

--- public/js/vue.js
+++
@@ -1,9 +0,0 @@
-require('../../resources/js/bootstrap');
-
-window.Vue = require('vue');
-
-Vue.component('category-component', require('../../resources/js/components/CategoryComponent.vue').default);
-
-const app = new Vue({
-    el: '#app',
-});
