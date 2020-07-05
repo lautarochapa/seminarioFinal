05eb5949e188c108d00bdf249dfa146e29aa5eae
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-05 16:29:45 -0300

    api + vistas

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function profile()
     {
-        return $this->belongsTo(Profile::class, 'nivel_acceso');
+        return $this->belongsTo(Profile::class, 'nivel_acceso')->nombre();
     }
 
     public function products()
