549ef273015909122be483cc2770cd09e3a9686a
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-05 20:05:06 -0300

    seed db

--- app/Providers/SocialGoogleAccountService.php
+++ app/Providers/SocialGoogleAccountService.php
@@ -11,24 +11,27 @@ class SocialGoogleAccountService
         $account = SocialGoogleAccount::whereProvider('google')
             ->whereProviderUserId($providerUser->getId())
             ->first();
-if ($account) {
-            return $account->user;
-        } else {
-$account = new SocialGoogleAccount([
-                'provider_user_id' => $providerUser->getId(),
-                'provider' => 'google'
-            ]);
-$user = User::whereEmail($providerUser->getEmail())->first();
-if (!$user) {
-$user = User::create([
-                    'email' => $providerUser->getEmail(),
-                    'name' => $providerUser->getName(),
-                    'password' => md5(rand(1,10000)),
+
+            if ($account) {
+                return $account->user;
+            } else {
+                $account = new SocialGoogleAccount([
+                    'provider_user_id' => $providerUser->getId(),
+                    'provider' => 'google'
                 ]);
+                $user = User::whereEmail($providerUser->getEmail())->first();
+                if (!$user){
+                    $user = User::create([
+                        'email' => $providerUser->getEmail(),
+                        'username' => $providerUser->getEmail(),
+                        'name' => $providerUser->getName(),
+                        'lastname' => $providerUser->getName(),
+                        'password' => md5(rand(1,10000)),
+                    ]);
+                }
+                $account->user()->associate($user);
+                $account->save();
+                return $user;
             }
-$account->user()->associate($user);
-            $account->save();
-return $user;
-        }
     }
 }
