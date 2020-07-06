4558f81577c1a1b1977c53a22522d7dbcff370af
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-05 22:28:42 -0300

    seed db

--- app/Http/Controllers/UtensilController.php
+++ app/Http/Controllers/UtensilController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Utensil;
+use Symfony\Component\HttpFoundation\Response;
 
 class UtensilController extends Controller
 {
     function getAll(){
-        return Utensil::all();
+      $utensils = Utensil::all();
+      return response()->json([
+      
+        'utensils' => $utensils
+      
+      ], Response::HTTP_OK);
       }
 }
