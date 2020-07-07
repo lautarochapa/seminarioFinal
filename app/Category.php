ead76eb48834c2d93be136f1dae5fb39e0999e69
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-07 15:27:31 -0300

    seed db

--- app/Category.php
+++ app/Category.php
@@ -21,7 +21,7 @@ class Category extends Model
     }
 
     public function allchildren() {
-        return $this->children()->with('allchildren')
+        return $this->children()->with('allchildren');
     }
 
 
