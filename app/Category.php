90295e41e6784755f40fb27d8f9074b58faab852
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-07 14:56:38 -0300

    seed db

--- app/Category.php
+++ app/Category.php
@@ -13,10 +13,9 @@ class Category extends Model
     }
 
     public function children() {
-        return $this->hasMany(Category::class,'padre')->withCount(['products' => function ($query) {
-            $query->withFilters();
-        }]);
+        return $this->hasMany(Category::class,'padre');
     }
+    // ->withCount(['products' => function ($query) {$query->withFilters(); }])
 
     public function allchildren() {
         return $this->children()->with('allchildren')->with(['supplies' => function($query){
