61e6cbce86637c037b29001ca7587611d67738b1
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-07 18:46:58 -0300

    seed db

--- public/js/app.js
+++ public/js/app.js
@@ -2347,6 +2347,18 @@ __webpack_require__.r(__webpack_exports__);
     return {
       supplies2: []
     };
+  },
+  mounted: function mounted() {
+    console.log("treviewingggggggggggg111");
+    var toggler = document.getElementsByClassName("caret");
+    var i;
+
+    for (i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function () {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+      });
+    }
   }
 });
 
