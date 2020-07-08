95091dbd9229014fa18598332bae9cc6ca7363cc
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-07 23:48:01 -0300

    seed db

--- resources/views/profiles/admin/product.blade.php
+++ resources/views/profiles/admin/product.blade.php
@@ -10,11 +10,6 @@
 
                   <div class="caption">
                       <h3> {{$product->nombre}} </h3>
-                    <!--  <p class="discription"> {{$product->discription}} </p>
-                      <div class="clearfix">
-                          <div class="pull-left price"/>$ {{$product->price}}</div>
-                          <a href= {{ route('product.addToCart', ['id' => $product->id ]) }} class="btn btn-danger pull-right" role="button">Add to cart</a>
-                  </div>-->
 
                    </div>
     </div>
