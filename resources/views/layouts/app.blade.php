8e597aec535d464cf65a2bb52c5663ba7239a7fc
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-11 17:40:34 -0300

    styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/treeview.css') }}" rel="stylesheet">
+   <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+   <!-- <link href="{{ asset('css/treeview.css') }}" rel="stylesheet"> -->
 </head>
 <body>
     <div id="app">
