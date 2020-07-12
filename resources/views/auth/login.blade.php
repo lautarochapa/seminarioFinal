6be3b868d6e6c058adc16690bda26e5ed90e24a6
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-11 23:00:52 -0300

    nav

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,26 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <div class="col-md-12">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                    <div class="col-md-6">
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
                         </div>
 
 
@@ -31,7 +25,6 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -39,11 +32,9 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -51,11 +42,9 @@
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
-                            </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
@@ -65,23 +54,19 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
-                            </div>
                         </div>
+                    </div>
 
+                    <div class="col-md-6">
 
-
-                        <br />
-                        <p style="margin-left:265px">OR</p>
-                        <br />
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                               <a href="{{url('/redirect')}}" class="btn btn-primary">Login with Google</a>
                             </div>
                         </div>
+                    </div>
 
-                    </form>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
