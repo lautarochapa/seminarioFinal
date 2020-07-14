7a170a9fb58a530467bd4112196b111800b9eac5
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-14 19:43:53 -0300

    style

--- resources/js/components/Front.vue
+++ resources/js/components/Front.vue
@@ -149,7 +149,7 @@
 
                 <div class="row mt-4">
 
-                    <div class="col-lg-4 col-md-6 mb-4" style="padding: 1%;" v-for="product in products.data">
+                    <div class="col-lg-4" style="padding: 1%;" v-for="product in products.data">
 
                         <productCard :prod="product"></productCard>
 
