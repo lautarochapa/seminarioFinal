dd44b34bd0493d995dd4d534d4f692b27964aab5
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-14 19:27:00 -0300

    style

--- resources/js/components/ProductCard.vue
+++ resources/js/components/ProductCard.vue
@@ -74,10 +74,34 @@ button {
 }
 
 .container .card:hover {
-/*    background: rgba(4,172,133, 0.2);
+/*    background: rgba(4,172,133, 0.1);
 */
     box-shadow: 2px 2px 10px rgba(0,0,0,0.6);
 }
+
+
+
+.container .card:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(4,172,133, 0.1);
+    clip-path: circle(150px at 80% -40%);
+    transition: 0.5s ease-in-out;
+}
+
+
+.container .card:hover:before{
+    clip-path: circle(400px at 30% 45%);
+
+   /* clip-path: circle(300px at 80% -20%);*/
+
+}
+
+
 
 .container .card .head .likes,
 .container .card .head{
