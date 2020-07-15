42f62491e831cd8cc075b102ee708333f8087b2b
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-15 20:55:40 -0300

    implementacion pipelines

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,8 +1,8 @@
 <table>
     @foreach($products as $prod)
         <tr>
-            <td>{{ $product->nombre}}</td>
-            <td>{{ $product->supply_id}}</td>
+            <td>{{ $prod->nombre}}</td>
+            <td>{{ $prod->supply_id}}</td>
         </tr>
     @endforeach
 </table>
