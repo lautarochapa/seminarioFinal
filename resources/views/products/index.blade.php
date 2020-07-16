12c9edfc5324dd83eb5fcfb398c4e6a45ab95337
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-15 22:01:46 -0300

    implementacion pipelines

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,6 @@
-<table>
+@extends('layouts.app')
 
-
+@section('content')
 <div class="row mt-4">
 
     <div class="col-lg-4 col-md-6 mb-4" style="padding: 1%;">
@@ -10,12 +10,4 @@
     </div>
 </div>
 
-
-   <!-- @foreach($products as $prod)
-        <tr>
-            <td>{{ $prod->nombre}}</td>
-            <td>{{ $prod->supply_id}}</td>
-        </tr>
-    @endforeach -->
-
-</table>
+@endsection
