7b0a20d15c6f4abe8c90cc657f454a394ee5aa14
lautaro Chiappero <lautarochapa@gmail.com>
2020-07-19 18:45:00 -0300

    mapa sucursales

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.app')
-
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
    crossorigin=""/>
@@ -9,14 +7,9 @@
    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
    crossorigin=""></script>
 
-@section('content')
-<div>
-
+<body>
     <div id="mapid" height="180px"></div>
-</div>
-
-@endsection
-
+</body>
 
 <script>
 
