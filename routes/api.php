e58a6d06c12328b3eca0b42d108294bba32f9bab
lautarochapa <lautarochapa@gmail.com>
2026-06-17 14:53:30 -0300

    Api Confirmar compra y actualizar stock

--- routes/api.php
+++ routes/api.php
@@ -643,6 +643,8 @@ Route::prefix('v1')->middleware(['web', 'trace_id', 'api_token', 'auth'])->group
 Route::prefix('v1')->middleware(['web', 'trace_id', 'api_token', 'auth'])->group(function () {
     Route::get('family-groups/{id}/purchases',                                          [\App\Http\Controllers\Api\V1\Purchases\PurchaseController::class, 'index']);
     Route::post('family-groups/{id}/purchases',                                         [\App\Http\Controllers\Api\V1\Purchases\PurchaseController::class, 'store']);
+    Route::post('family-groups/{id}/purchases/{purchaseId}/confirm',                    [\App\Http\Controllers\Api\V1\Purchases\PurchaseConfirmationController::class, 'confirm']);
+    Route::post('family-groups/{id}/purchases/{purchaseId}/add-to-stock',               [\App\Http\Controllers\Api\V1\Purchases\PurchaseConfirmationController::class, 'addToStock']);
     Route::get('family-groups/{id}/purchases/{purchaseId}/items',                       [\App\Http\Controllers\Api\V1\Purchases\PurchaseItemController::class, 'index']);
     Route::post('family-groups/{id}/purchases/{purchaseId}/items',                      [\App\Http\Controllers\Api\V1\Purchases\PurchaseItemController::class, 'store']);
     Route::patch('family-groups/{id}/purchases/{purchaseId}/items/{itemId}',            [\App\Http\Controllers\Api\V1\Purchases\PurchaseItemController::class, 'update']);
