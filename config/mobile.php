6daa2612854e76aea4e0c502d7d6605bb44f51cf
lautarochapa <lautarochapa@gmail.com>
2026-09-23 13:54:55 -0300

    release: publicar descarga Android 1.0.7 (8) [skip render]

--- config/mobile.php
+++ config/mobile.php
@@ -2,11 +2,11 @@
 
 return [
     'android' => [
-        'version' => '1.0.6',
-        'build' => 7,
-        'size_bytes' => 129110371,
-        'sha256' => '7d6258973e7270f91c5472f1ad0c930464711b275c6dfdc41110132e91d9a81e',
-        'download_url' => env('ANDROID_APK_URL', 'https://github.com/lautarochapa/seminarioFinal/releases/download/android-v1.0.6-7/CocinaComidaControl-1.0.6-7.apk'),
+        'version' => '1.0.7',
+        'build' => 8,
+        'size_bytes' => 129114783,
+        'sha256' => '5d29dbba7d6756bafac4fdca8fd7d5d71903ba1de140ce0dadbed0f27edda585',
+        'download_url' => env('ANDROID_APK_URL', 'https://github.com/lautarochapa/seminarioFinal/releases/download/android-v1.0.7-8/CocinaComidaControl-1.0.7-8.apk'),
         'download_page_url' => env('ANDROID_DOWNLOAD_PAGE_URL', 'https://cocinacomidacontrol.com.ar/#descarga-app'),
     ],
 ];
