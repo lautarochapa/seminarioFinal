e1f4ef99fe185569f48e1b0347ce4dedaca6fed3
lautarochapa <lautarochapa@gmail.com>
2026-09-24 02:46:50 -0300

    Publish Android 1.0.9 download and version metadata

--- config/mobile.php
+++ config/mobile.php
@@ -2,11 +2,11 @@
 
 return [
     'android' => [
-        'version' => '1.0.8',
-        'build' => 9,
-        'size_bytes' => 128749779,
-        'sha256' => '361db97d6bf2be54143aca0f44bd0cb60577044433f1ee40588347a5388d56e3',
-        'download_url' => env('ANDROID_APK_URL', 'https://github.com/lautarochapa/seminarioFinal/releases/download/android-v1.0.8-9/CocinaComidaControl-1.0.8-9.apk'),
+        'version' => '1.0.9',
+        'build' => 10,
+        'size_bytes' => 128750647,
+        'sha256' => 'aacafbad722858f708f146bb82d70c02aa6822aa5c01271e9da20c9a292b9cc9',
+        'download_url' => env('ANDROID_APK_URL', 'https://github.com/lautarochapa/seminarioFinal/releases/download/android-v1.0.9-10/CocinaComidaControl-1.0.9-10.apk'),
         'download_page_url' => env('ANDROID_DOWNLOAD_PAGE_URL', 'https://cocinacomidacontrol.com.ar/#descarga-app'),
     ],
 ];
